845b47bf5e2d4e2394ea3e6113ea1410fea49049
Parameshwar Reddy Patlolla <patlollaparameshwarreddy7@gmail.com>
2019-01-29 19:36:23 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           SingletonPattern reference1 = SingletonPattern.GetInstance;
+            reference1.Message("hi how are you");
+            SingletonPattern reference2 = SingletonPattern.GetInstance;
+            reference2.Message("manju");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ SingletonPattern.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns
+{
+    class SingletonPattern
+    {
+        static int numberOfInstances = 0;
+        private static SingletonPattern instance = null;
+        private SingletonPattern()
+        {
+            numberOfInstances++;
+            Console.WriteLine("number of instances created is " + numberOfInstances);
+        }
+
+        public static SingletonPattern GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new SingletonPattern();
+                }
+
+                return instance;
+            }                   
+        }
+
+        public void Message(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+    }
+}
